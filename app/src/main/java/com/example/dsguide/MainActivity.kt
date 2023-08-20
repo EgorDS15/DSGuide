b76ef75532bac214e69e1f0f65c0e818300d2264
EgorDS15 <ovchinnikovegords@gmail.com>
2023-08-20 19:18:07 +0300

    initial commit

---
+++ app/src/main/java/com/example/dsguide/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.dsguide
+
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.airbnb.lottie.LottieAnimationView
+import kotlinx.coroutines.delay
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val anima = findViewById<LottieAnimationView>(R.id.logoSplash)
+
+
+    }
+}
+
